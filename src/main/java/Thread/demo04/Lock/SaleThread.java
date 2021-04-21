ce46e25fada1a36eae80c890db60d8d51731afce
Chr1sM1 <mzs0119@qq.com>
2021-04-21 17:52:20 +0800

    day2

---
+++ src/main/java/Thread/demo04/Lock/SaleThread.java
@@ -0,0 +1,19 @@
+package Thread.demo04.Lock;
+
+/**
+ * @ClassName SaleThread
+ * @Description TODO
+ * @Author Chris
+ * @Date 2021/4/20
+ **/
+public class SaleThread {
+    public static void main(String[] args) {
+        Tickets t = new Tickets();
+        Thread t1 = new Thread(t);
+        Thread t2 = new Thread(t);
+        Thread t3 = new Thread(t);
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+}
