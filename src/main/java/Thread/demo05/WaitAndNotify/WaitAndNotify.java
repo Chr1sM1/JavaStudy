a2f01bb61f9693ebb7221a34aa5fcd3110d5cb65
Chr1sM1 <mzs0119@qq.com>
2021-04-21 20:28:13 +0800

    解决死锁问题

--- src/main/java/Thread/demo05/WaitAndNotify/WaitAndNotify.java
+++ src/main/java/Thread/demo05/WaitAndNotify/WaitAndNotify.java
@@ -46,6 +46,7 @@ public class WaitAndNotify {
                         //唤醒之后执行的代码
                         System.out.println("包子已经做好了，开吃！");
                         System.out.println("------------------");
+                        obj.notify();
                     }
                 }
             }
@@ -56,17 +57,18 @@ public class WaitAndNotify {
             @Override
             public void run() {
                 while(true){
-                    try {
-                    Thread.sleep(3000);//花3秒种做包子
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                synchronized (obj){
-                    //保证等待和唤醒的线程只有一个执行，需要使用同步技术
-                    System.out.println("老板3秒钟之后做好了包子，告知顾客，可以吃包子了");
-                    //做好包子之后，调用notify方法，唤醒顾客吃包子
-                    obj.notify();
-                }
+                    synchronized (obj){
+                        try {
+                            Thread.sleep(3);//花3秒种做包子
+                            // 保证等待和唤醒的线程只有一个执行，需要使用同步技术
+                             System.out.println("老板3秒钟之后做好了包子，告知顾客，可以吃包子了");
+                            // 做好包子之后，调用notify方法，唤醒顾客吃包子
+                             obj.notify();
+                             obj.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    } 
                 }
             }
         }.start();
